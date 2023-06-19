3a1e2c23ee3a96b7b9bf97ac3c2012530ff26505
Evgeniy <evgnec@gmail.com>
2023-06-19 22:30:02 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,19 @@ import Painting from './Painting';
 export const App = () => {
   return (
     <div>
-        <Painting
-    imgUrl={paintings[0].url}
-    title={paintings[0].title}
-    authorName={paintings[0].author.tag}
-    profileUser={paintings[0].author.url}
-    price={paintings[0].price}
-    qty={paintings[0].quantity}
-      />
-              <Painting
-    imgUrl={paintings[1].url}
-    title={paintings[1].title}
-    authorName={paintings[1].author.tag}
-    profileUser={paintings[1].author.url}
-    price={paintings[1].price}
-    qty={paintings[1].quantity}
-  />
+      {paintings.map(
+        painting => 
+      
+<Painting
+    imgUrl={painting.url}
+    title={painting.title}
+    authorName={painting.author.tag}
+    profileUser={painting.author.url}
+    price={painting.price}
+    qty={painting.quantity}
+      />) 
+      }
+       
     </div>
   );
 };
