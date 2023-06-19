5593c27eba98bac0cb58926728a4b8394fb8b363
Evgeniy <evgnec@gmail.com>
2023-06-19 22:53:16 +0300

    add PropTypes

--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -1,9 +1,8 @@
-import PropTypes from 'prop-types'
-import defaultImage from '../default.jpg'
-
+import PropTypes from 'prop-types';
+import defaultImage from '../default.jpg';
 
 const Painting = ({
-  imgUrl =defaultImage,
+  imgUrl = defaultImage,
   title,
   authorName = 'noname',
   profileUser,
@@ -18,7 +17,10 @@ const Painting = ({
         Autor: <a href={profileUser}>{authorName}</a>
       </p>
       <p>Price: {price} $</p>
-          <p> Доступность: {qty < 10 ? 'end' : 'start'} {qty}</p>
+      <p>
+        {' '}
+        Доступность: {qty < 10 ? 'end' : 'start'} {qty}
+      </p>
       <button type="button">Add bascet</button>
     </div>
   );
@@ -27,5 +29,10 @@ const Painting = ({
 export default Painting;
 
 Painting.propTypes = {
-    imgUrl: PropTypes.string,
-}
+    imgUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    profileUser: PropTypes.string.isRequired,
+        authorName: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    qty: PropTypes.number.isRequired,
+};
